770bba22c6b0515d0889025be58af1cb00ffb138
igorokopylov <66507754+igorokopylov@users.noreply.github.com>
2020-07-23 13:49:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ $(document).ready(function () {
         $('.header__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    $('.button_primary_filter').click(function (event) {
+        $('body').toggleClass('lock');
+    });
 });
